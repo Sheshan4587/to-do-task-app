245ae7fe7a414d72c7bd2675275c0671f4694e45
Sheshan4587 <sheshanthisal3@gmail.com>
2026-03-13 11:52:00 +0530

    Add POST route to extract from data

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const app = express();
 
-app.use("/static", express.static("public"));  
+app.use(express.urlencoded({ extended: true })); //middleware to parse the body of POST requests
+
+app.use("/static", express.static("public"));
 
 app.set("view engine", "ejs"); //set the view engine to ejs, which allows us to render ejs templates in our routes
 
@@ -10,6 +12,12 @@ app.get('/', (req, res) => {
     res.render('todo.ejs');  //send "Hello World!" as the response when the root URL is accessed
 });
 
+app.post('/', (req, res) => {
+    console.log(req.body);  //log the body of the POST request to the console
+});
+
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');   //log a message to the console when the server starts listening on port 3000
 });
+
+

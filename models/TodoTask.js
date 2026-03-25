30a2eae83708339f800f045982b0a718a5e67cd3
Sheshan4587 <sheshanthisal3@gmail.com>
2026-03-25 18:21:41 +0530

    Enhance comments in TodoTask model for better clarity on schema fields

--- models/TodoTask.js
+++ models/TodoTask.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose'); // Import Mongoose to define the schema and model for the to-do tasks
 
 // Define the schema for a to-do task, which includes a "task" field of type String
+// The "task" field is required, meaning that it cannot be empty when creating a new to-do task
+// The schema also includes a "date" field of type Date, which defaults to the current date and time when a new to-do task is created
+// This schema will be used to create a Mongoose model, which allows us to interact with the "TodoTask" collection in MongoDB using this model
 const todoTaskSchema = new mongoose.Schema({
     content: {
         type: String,

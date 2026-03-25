9e0e81079513eebd0e8c6c04e851361ab3bd1ed2
Sheshan4587 <sheshanthisal3@gmail.com>
2026-03-25 18:02:05 +0530

    Add dotenv configuration and MongoDB connection details

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const app = express();
 
+const dotenv = require('dotenv'); // Load environment variables from .env file
+
 app.use(express.urlencoded({ extended: true })); //middleware to parse the body of POST requests
 
-app.use("/static", express.static("public"));
+app.use("/static", express.static("public"));  //middleware to serve static files from the "public" directory when the URL starts with "/static". For example, if you have a file called "style.css" in the "public" directory, you can access it at "http://localhost:3000/static/style.css".
 
 app.set("view engine", "ejs"); //set the view engine to ejs, which allows us to render ejs templates in our routes
 
